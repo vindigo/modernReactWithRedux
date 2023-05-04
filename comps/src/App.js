2f1ffb6e92eca74b8f4f2bc73d600a74acb0512b
rfrausto <vindigo@gmail.com>
2023-05-04 10:25:49 -0700

    feat: accordion done

--- comps/src/App.js
+++ comps/src/App.js
@@ -1,48 +1,26 @@
-import Button from './components/Button';
-import { GoBell, GoCloudDownload, GoDatabase } from 'react-icons/go';
+import Accordion from './components/Accordion';
+
 
 function App() {
-  const handleClick = () => {
-    console.log("clicked");
-  }
+  const items = [
+    {
+      id: 1,
+      label: "Alpha",
+      content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque placerat vitae lorem ut luctus. Morbi pharetra dui quis imperdiet venenatis. Aliquam erat volutpat. Lorem ipsum dolor sit amet, consectetur adipiscing elit. "
+    },
+    {
+      id: 2,
+      label: "Sigma",
+      content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque placerat vitae lorem ut luctus. Morbi pharetra dui quis imperdiet venenatis. Aliquam erat volutpat. Lorem ipsum dolor sit amet, consectetur adipiscing elit. "
+    },
+    {
+      id: 3,
+      label: "Omega",
+      content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque placerat vitae lorem ut luctus. Morbi pharetra dui quis imperdiet venenatis. Aliquam erat volutpat. Lorem ipsum dolor sit amet, consectetur adipiscing elit. "
+    }
+  ];
 
-  return (
-    <div className="p-3">
-      <div>
-        <Button 
-          success 
-          rounded 
-          onClick={handleClick}
-          className="mb-5">
-            <GoCloudDownload />
-            Click me!!
-        </Button>
-      </div>
-      <div>
-        <Button danger>
-          <GoDatabase />
-          Buy Now!
-        </Button>
-      </div>
-      <div>
-        <Button warning>
-          <GoBell />
-          See Deal!
-        </Button>
-      </div>
-      <div>
-        <Button secondary>
-          Hide Ads!
-        </Button>
-      </div>
-      <div>
-        <Button primary rounded>
-          Something!
-        </Button>
-      </div>
-  
-    </div>
-  );
+  return <Accordion items={items}/>
 }
 
 export default App;
---
+++ comps/src/pages/ButtonPage.js
@@ -0,0 +1,48 @@
+import Button from '../components/Button';
+import { GoBell, GoCloudDownload, GoDatabase } from 'react-icons/go';
+
+function ButtonPage() {
+  const handleClick = () => {
+    console.log("clicked");
+  }
+
+  return (
+    <div className="p-3">
+      <div>
+        <Button 
+          success 
+          rounded 
+          onClick={handleClick}
+          className="mb-5">
+            <GoCloudDownload />
+            Click me!!
+        </Button>
+      </div>
+      <div>
+        <Button danger>
+          <GoDatabase />
+          Buy Now!
+        </Button>
+      </div>
+      <div>
+        <Button warning>
+          <GoBell />
+          See Deal!
+        </Button>
+      </div>
+      <div>
+        <Button secondary>
+          Hide Ads!
+        </Button>
+      </div>
+      <div>
+        <Button primary rounded>
+          Something!
+        </Button>
+      </div>
+  
+    </div>
+  );
+}
+
+export default ButtonPage;
